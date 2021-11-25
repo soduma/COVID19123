930f20d08255ce86985e0230b5652cdcdc434a37
soduma <soduma@naver.com>
2021-11-25 10:09:50 +0900

    Initial Commit

---
+++ COVID19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID19
+//
+//  Created by 장기화 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
